840e76941638b17467922c49c85c4e2e93372e4a
johnbreth <jbreth1@gmail.com>
2022-12-19 21:25:18 -0500

    Added Second Application EPG

--- Tenant Configuration/main.tf
+++ Tenant Configuration/main.tf
@@ -64,3 +64,22 @@ resource "aci_application_epg" "CyberInsight_Security_application_epg" {
     prio                              = "unspecified"
     shutdown                      = "no"
 }
+
+#Create second EPG
+resource "aci_application_epg" "CyberInsight_Linux_application_epg" {
+    application_profile_dn  = aci_application_profile.CyberInsight_ap.id
+    name                              = "CyberInsight_Linux_application_epg"
+    description                   = "from terraform"
+    annotation                    = "tag_epg"
+    exception_tag                 = "0"
+    flood_on_encap            = "disabled"
+    fwd_ctrl                      = "none"
+    has_mcast_source             = "no"
+    is_attr_based_epg         = "no"
+    match_t                          = "AtleastOne"
+    name_alias                  = "CyberInsight_Linux_epg"
+    pc_enf_pref                  = "unenforced"
+    pref_gr_memb                  = "exclude"
+    prio                              = "unspecified"
+    shutdown                      = "no"
+}
